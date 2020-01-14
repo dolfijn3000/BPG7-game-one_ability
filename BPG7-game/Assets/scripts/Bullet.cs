3993b0532648b836ca21ec12f1473e7a63f0298f
dolfijn3000 <yente.paternotte@hotmail.com>
2020-01-14 19:59:39 +0100

    added chooting machanic
    Added a charcter that can shoot bullets. this bullets can be used as the head of the grapling hook. when it colides with somthing we can freeze and move the player to that possition.

---
+++ BPG7-game/Assets/scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.layer.ToString() == "grapable")
+        {
+            Destroy(gameObject);
+        }
+        //GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        
+    }
+}
---
+++ BPG7-game/Assets/scripts/FolowSprite.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FolowSprite : MonoBehaviour
+{
+
+    public Transform player;
+
+    private void FixedUpdate()
+    {
+
+        transform.position = new Vector3(player.position.x, player.position.y, -10);
+    }
+}
+
