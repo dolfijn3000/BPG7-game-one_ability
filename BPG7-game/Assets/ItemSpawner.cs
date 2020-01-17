3975f40e05affe29316bd3f20c9dca0523b92bed
dolfijn3000 <yente.paternotte@hotmail.com>
2020-01-17 17:01:16 +0100

    commit

---
+++ BPG7-game/Assets/ItemSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    [SerializeField] Bullet[] bulletTypes;
+    Random r = new Random();
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
